c47e7cb33b00dec50235fd14013d3fb58a6701e2
mattc-27 <mattcopeland927@gmail.com>
2023-12-27 16:51:14 -0700

    remove test files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import React from 'react';
+import './style.css'
 
-import {
-    createRoutesFromElements,
-    Route,
-    createBrowserRouter,
-    RouterProvider,
-} from 'react-router-dom';
-
-import './style.css';
-
-import { Layout } from './components/layout';
-import { Home } from './Home';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout />,
-        children: [
-         {
-            children: [
-                {index: true, element: <Home />}
-            ]
-         }
-        ]
-    }
-])
 
 export default function App() {
 
     return (
-        <RouterProvider router={router} />
+        <div className='main'>
+            <div className='title'>
+                <h1>Hello world</h1>
+            </div>
+        </div>
     );
 }
