8e7fd0cb6b17969be311e41798af48bacedefd5c
mattc-27 <mattcopeland927@gmail.com>
2023-12-27 19:14:29 -0700

    test updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,28 @@ import './style.css'
 export default function App() {
 
     return (
-        <div className='main'>
-            <div className='title'>
-                <h1>Hello world</h1>
+        <>
+            <header>
+                <div className='header-content'>
+                    <div className='header-logo'>
+                        <h2>PurRoots</h2>
+                    </div>
+                    <nav>
+                        <a className='nav-link' href='/'>Home</a>
+
+                    </nav>
+                </div>
+            </header>
+            <div className='main'>
+                <div className='title'>
+                    <h1>Hello world</h1>
+                </div>
             </div>
-        </div>
+        </>
+        /* <div className='main'>
+                 <div className='title'>
+                     <h1>Hello world</h1>
+                 </div>
+             </div> */
     );
 }
